988d0ab740c8a767d0e097b45ca524cb52792a5c
NitsuaMa <jurgensaj@gmail.com>
2026-04-26 18:11:37 -0400

    fixed the build configuration to resolve the artifact upload error

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,10 @@ export default defineConfig(({mode}) => {
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
     },
+    build: {
+      outDir: "dist",
+      emptyOutDir: false
+    },
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
